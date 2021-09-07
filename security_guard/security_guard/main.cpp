989d2e2d7b82535faaa4c60b9797e026f0fed4ee
Doga <61098483+dogaegeozden@users.noreply.github.com>
2021-09-06 20:33:05 -0700

    Add files via upload

---
+++ security_guard/security_guard/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int age;
+    string idCheck;
+
+    cout<<"Hey, how old are you?\n";
+    cin>>age;
+
+    if ( age <= 18 ) {
+        cout<<"Go away!";
+    } else if ( age > 18 ) {
+        cout<<"Can I see your id?\n";
+        cin>>idCheck;
+        if ( idCheck.find("yes")!=-1 || idCheck.find("Yes")!=-1 ) {
+            cout<<"You can get in.\n";
+        } else {
+            cout<<"Get out of the line. You can't get in.\n";
+            cin.ignore();
+        }
+    }
+}
+
